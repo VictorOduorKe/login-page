55706bcc8ac2d5fcf0a841e0c5c0fa2a5e1c003b
VictorOduorKe <88191160+VictorOduorKe@users.noreply.github.com>
2025-03-22 14:32:28 +0300

    deleted unneccesary data

--- react-login/src/App.jsx
+++ react-login/src/App.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './Components/Register';
 import Login from './Components/Login';
-import NavBr from './Components/NavBr';
 import "./App.css";
 import ResetPassword from './Components/ResetPassword';
 
 const App = () => {
   return (
-    <Router> {/* Wrap the entire app in Router */}
+    <Router>
       <div className="app">
         <Routes>
           <Route path="/" element={<Register />} />
